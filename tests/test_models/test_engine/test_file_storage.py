71c6475848d1c1520c764475b2ab3633d12087ee
Dohessiekan <d.gnondoyi@alustudent.com>
2023-09-17 19:05:32 +0200

    Good

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,28 +1,17 @@
 #!/usr/bin/python3
 import unittest
-from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models import storage
 
-# Instantiate the storage object
-storage = FileStorage()
 
+class TestFileStorage(unittest.TestCase):
+    ''' Test File storage '''
 
-class TestBase(unittest.TestCase):
-    def test_class_properties(self):
-        self.assertIsInstance(storage, FileStorage)
-        self.assertEqual(hasattr(FileStorage, "_FileStorage__file_path"), True)
-        self.assertEqual(hasattr(FileStorage, "_FileStorage__objects"), True)
-
-    def test_all(self):
-        self.assertIsInstance(storage.all(), dict)
-
-    def test_new(self):
-        base = BaseModel()
-        key = key = "{}.{}".format(base.__class__.__name__, base.id)
-        self.assertIn(key, storage.all().keys())
-
-    def test_reload(self):
-        self.assertIsInstance(storage.reload(), dict)
+    def test_FileStorage(self):
+        """test_FileStorage"""
+        bm = BaseModel()
+        self.assertIn("BaseModel." + bm.id, storage.all().keys())
 
 
 if __name__ == "__main__":
